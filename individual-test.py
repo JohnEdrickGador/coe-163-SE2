3fe261c205622cdf2de48265878c081203d4799f
JohnEdrickGador <jmgador@up.edu.ph>
2023-05-04 17:39:19 +0800

    Upgraded testing script to test all arrangements at once for every size

--- individual-test.py
+++ individual-test.py
@@ -4,7 +4,6 @@ import copy
 
 random.seed(0)
 
-n = 10
 
 def init_rand(mat,n):
   for x in range(n):
@@ -28,13 +27,14 @@ def fill_zero(mat,n):
 A = []
 B = []
 C = []
+n = 50
 print("Matrix size is %d-by-%d" % (n,n))
 
 init_rand(A,n)
 init_rand(B,n)
 init_matrix(C,n,0)
 
-record = open("small.txt","w+")
+record1 = open("ijk.txt","w+")
 
 for x in range(100):
     ctmp = copy.deepcopy(C) #changes in original array wont affect ctmp
@@ -45,7 +45,76 @@ for x in range(100):
                 ctmp[i][j] = ctmp[i][j] + A[i][k]*B[k][j]
     runtime = time() - t
     print("ijk completed in %f seconds" % (runtime))
-    record.write(f"{runtime}\n")
+    record1.write(f"{runtime}\n")
     fill_zero(C,n)
+record1.close()
   
+record2 = open("kji.txt","w+")
+for x in range(100):
+    ctmp = copy.deepcopy(C)
+    t = time()
+    for k in range(n):
+        for j in range(n):
+            for i in range(n):
+                ctmp[i][j] = ctmp[i][j] + A[i][k]*B[k][j]
+    runtime = time() - t
+    print("kji completed in %f seconds" % (runtime))
+    record2.write(f"{runtime}\n")
+    fill_zero(C,n)
+record2.close()
+
+record3 = open("jik.txt","w+")
+for x in range(100):
+    ctmp = copy.deepcopy(C)
+    t = time()
+    for j in range(n):
+        for i in range(n):
+            for k in range(n):
+                ctmp[i][j] = ctmp[i][j] + A[i][k]*B[k][j]
+    runtime = time() - t
+    print("jik completed in %f seconds" % (runtime))
+    record3.write(f"{runtime}\n")
+    fill_zero(C,n)
+record3.close()
+
+record4 = open("jki.txt","w+")
+for x in range(100):
+    ctmp = copy.deepcopy(C)
+    t = time()
+    for j in range(n):
+        for k in range(n):
+            for i in range(n):
+                ctmp[i][j] = ctmp[i][j] + A[i][k]*B[k][j]
+    runtime = time() - t
+    print("jki completed in %f seconds" % (runtime))
+    record4.write(f"{runtime}\n")
+    fill_zero(C,n)
+record4.close()
+
+record5 = open("kij.txt","w+")
+for x in range(100):
+    ctmp = copy.deepcopy(C)
+    t = time()
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                ctmp[i][j] = ctmp[i][j] + A[i][k]*B[k][j]
+    runtime = time() - t
+    print("kij completed in %f seconds" % (runtime))
+    record5.write(f"{runtime}\n")
+    fill_zero(C,n)
+record5.close()
+
+record6 = open("ikj.txt","w+")
+for x in range(100):
+    ctmp = copy.deepcopy(C)
+    t = time()
+    for i in range(n):
+        for k in range(n):
+            for j in range(n):
+                ctmp[i][j] = ctmp[i][j] + A[i][k]*B[k][j]
+    runtime = time() - t
+    print("ikj completed in %f seconds" % (runtime))
+    fill_zero(C,n)
+record6.close()
 
